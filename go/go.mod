557bf2cc568584e27ba05734192a032b5a6ed11f
DuttaAB-dev <61903332+DuttaAB-dev@users.noreply.github.com>
2020-10-13 20:52:02 +0530

    Added desktop support

---
+++ go/go.mod
@@ -0,0 +1,8 @@
+module elements/go
+
+go 1.15
+
+require (
+	github.com/go-flutter-desktop/go-flutter v0.42.0
+	github.com/pkg/errors v0.9.1
+)
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-
 const kRowCount = 10;
 
 const kContentSize = 64.0;
@@ -11,30 +10,33 @@ const kGutterWidth = 2.0;
 
 const kGutterInset = EdgeInsets.all(kGutterWidth);
 
-
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  final gridList = rootBundle.loadString('assets/elementsGrid.json')
+  final gridList = rootBundle
+      .loadString('assets/elementsGrid.json')
       .then((source) => jsonDecode(source)['elements'] as List)
-      .then((list) => list.map((json) => json != null ? ElementData.fromJson(json) : null).toList());
+      .then((list) => list
+          .map((json) => json != null ? ElementData.fromJson(json) : null)
+          .toList());
 
   runApp(ElementsApp(gridList));
 }
 
-
 class ElementData {
-
   final String name, category, symbol, extract, source, atomicWeight;
   final int number;
   final List<Color> colors;
 
   ElementData.fromJson(Map<String, dynamic> json)
-      : name = json['name'], category = json['category'], symbol = json['symbol'],
-        extract = json['extract'], source = json['source'],
-        atomicWeight = json['atomic_weight'], number = json['number'],
+      : name = json['name'],
+        category = json['category'],
+        symbol = json['symbol'],
+        extract = json['extract'],
+        source = json['source'],
+        atomicWeight = json['atomic_weight'],
+        number = json['number'],
         colors = (json['colors'] as List).map((value) => Color(value)).toList();
 }
-
 
 class ElementsApp extends StatelessWidget {
   ElementsApp(this.gridList);
@@ -46,12 +48,14 @@ class ElementsApp extends StatelessWidget {
     final theme = ThemeData(
       brightness: Brightness.dark,
       accentColor: Colors.grey,
-
-      textTheme: Typography.whiteMountainView.apply(fontFamily: 'Roboto Condensed'),
-      primaryTextTheme: Typography.whiteMountainView.apply(fontFamily: 'Share Tech Mono'),
+      textTheme:
+          Typography.whiteMountainView.apply(fontFamily: 'Roboto Condensed'),
+      primaryTextTheme:
+          Typography.whiteMountainView.apply(fontFamily: 'Share Tech Mono'),
     );
 
-    return MaterialApp(title: 'Elements', theme: theme, home: TablePage(gridList));
+    return MaterialApp(
+        title: 'Elements', theme: theme, home: TablePage(gridList));
   }
 }
 
@@ -64,23 +68,36 @@ class TablePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey[900],
-      appBar: AppBar(title: Text('Elements'), centerTitle: true, backgroundColor: Colors.blueGrey[800]),
+      appBar: AppBar(
+          title: Text('Elements'),
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey[800]),
       body: FutureBuilder(
         future: gridList,
-        builder: (_, snapshot) => snapshot.hasData ? _buildTable(snapshot.data)
+        builder: (_, snapshot) => snapshot.hasData
+            ? _buildTable(snapshot.data)
             : Center(child: CircularProgressIndicator()),
       ),
     );
   }
 
   Widget _buildTable(List<ElementData> elements) {
-    final tiles = elements.map((element) => element != null ? ElementTile(element)
-        : Container(color: Colors.black38, margin: kGutterInset)).toList();
+    final tiles = elements
+        .map((element) => element != null
+            ? ElementTile(element)
+            : Container(color: Colors.black38, margin: kGutterInset))
+        .toList();
 
     return SingleChildScrollView(
-      child: SizedBox(height: kRowCount * (kContentSize + (kGutterWidth * 2)),
-        child: GridView.count(crossAxisCount: kRowCount, children: tiles,
-          scrollDirection: Axis.horizontal,),),);
+      child: SizedBox(
+        height: kRowCount * (kContentSize + (kGutterWidth * 2)),
+        child: GridView.count(
+          crossAxisCount: kRowCount,
+          children: tiles,
+          scrollDirection: Axis.horizontal,
+        ),
+      ),
+    );
   }
 }
 
@@ -92,28 +109,34 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final listItems = <Widget>[
-      ListTile(leading: Icon(Icons.category), title : Text(element.category.toUpperCase())),
-      ListTile(leading: Icon(Icons.info), title : Text(element.extract),
-        subtitle: Text(element.source),),
-      ListTile(leading: Icon(Icons.fiber_smart_record), title: Text(element.atomicWeight),
-        subtitle: Text('Atomic Weight'),),
+      ListTile(
+          leading: Icon(Icons.category),
+          title: Text(element.category.toUpperCase())),
+      ListTile(
+        leading: Icon(Icons.info),
+        title: Text(element.extract),
+        subtitle: Text(element.source),
+      ),
+      ListTile(
+        leading: Icon(Icons.fiber_smart_record),
+        title: Text(element.atomicWeight),
+        subtitle: Text('Atomic Weight'),
+      ),
     ].expand((widget) => [widget, Divider()]).toList();
 
     return Scaffold(
       backgroundColor: Color.lerp(Colors.grey[850], element.colors[0], 0.07),
-
       appBar: AppBar(
         backgroundColor: Color.lerp(Colors.grey[850], element.colors[1], 0.2),
-        bottom: ElementTile(element, isLarge: true),),
-
+        bottom: ElementTile(element, isLarge: true),
+      ),
       body: ListView(padding: EdgeInsets.only(top: 24.0), children: listItems),
     );
   }
 }
 
-
 class ElementTile extends StatelessWidget implements PreferredSizeWidget {
-  const ElementTile(this.element, { this.isLarge = false });
+  const ElementTile(this.element, {this.isLarge = false});
 
   final ElementData element;
   final bool isLarge;
@@ -123,11 +146,17 @@ class ElementTile extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     final tileText = <Widget>[
-      Align(alignment: AlignmentDirectional.centerStart,
-        child: Text('${element.number}', style: TextStyle(fontSize: 10.0)),),
+      Align(
+        alignment: AlignmentDirectional.centerStart,
+        child: Text('${element.number}', style: TextStyle(fontSize: 10.0)),
+      ),
       Text(element.symbol, style: Theme.of(context).primaryTextTheme.headline),
-      Text(element.name, maxLines: 1, overflow: TextOverflow.ellipsis,
-        textScaleFactor: isLarge ? 0.65 : 1,),
+      Text(
+        element.name,
+        maxLines: 1,
+        overflow: TextOverflow.ellipsis,
+        textScaleFactor: isLarge ? 0.65 : 1,
+      ),
     ];
 
     final tile = Container(
@@ -136,21 +165,26 @@ class ElementTile extends StatelessWidget implements PreferredSizeWidget {
       height: kContentSize,
       foregroundDecoration: BoxDecoration(
         gradient: LinearGradient(colors: element.colors),
-        backgroundBlendMode: BlendMode.multiply,),
+        backgroundBlendMode: BlendMode.multiply,
+      ),
       child: RawMaterialButton(
-        onPressed: !isLarge ? () => Navigator.push(context,
-            MaterialPageRoute(builder: (_) => DetailPage(element))) : null,
+        onPressed: !isLarge
+            ? () => Navigator.push(
+                context, MaterialPageRoute(builder: (_) => DetailPage(element)))
+            : null,
         fillColor: Colors.grey[800],
         disabledElevation: 10.0,
         padding: kGutterInset * 2.0,
-        child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: tileText),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: tileText),
       ),
     );
 
     return Hero(
       tag: 'hero-${element.symbol}',
-      flightShuttleBuilder: (_, anim, __, ___, ____) =>
-          ScaleTransition(scale: anim.drive(Tween(begin: 1, end: 1.75)), child: tile),
+      flightShuttleBuilder: (_, anim, __, ___, ____) => ScaleTransition(
+          scale: anim.drive(Tween(begin: 1, end: 1.75)), child: tile),
       child: Transform.scale(scale: isLarge ? 1.75 : 1, child: tile),
     );
   }
