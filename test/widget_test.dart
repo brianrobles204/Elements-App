5b4be915331786ea5d3eca4269c717cf67f0f5d9
DuttaAB-dev <61903332+DuttaAB-dev@users.noreply.github.com>
2020-10-18 01:47:35 +0530

    new update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:Elements/main.dart';
+import 'package:Elements-App/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
